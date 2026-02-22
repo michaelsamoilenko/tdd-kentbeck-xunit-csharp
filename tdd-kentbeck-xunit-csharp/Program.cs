181dc97e2d50294645d6687adb630bb50db2dbd3
Michael <michaelsamoilenkood@gmail.com>
2026-02-22 10:15:15 +0200

    End of chapter 21

--- tdd-kentbeck-xunit-csharp/Program.cs
+++ tdd-kentbeck-xunit-csharp/Program.cs
@@ -1,13 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 
-new TestCaseTest("TestTemplateMethod").Run();
+new TestCaseTest("TestTemplateMethod").Run().Summary();
+new TestCaseTest("TestResult").Run().Summary();
+new TestCaseTest("TestFailedResult").Run().Summary();
 
 public class TestCase(string name)
 {
     public virtual void SetUp(){ }
     public virtual void TearDown() { }
-    public void Run()
+    public TestResult Run()
     {
+        var result = new TestResult();
+        result.TestStarted();
         SetUp();
         var method = GetType().GetMethod(name, 
             BindingFlags.Instance | 
@@ -15,6 +20,7 @@ public class TestCase(string name)
             BindingFlags.NonPublic);
         method.Invoke(this, null);
         TearDown();
+        return result;
     }
     // This helper method is basically simplified replication of what Python 'assert' does.
     // C#'s Debug.Assert doesn't suit us since it doesn't throw an exception but just reports an error.
@@ -26,6 +32,25 @@ public class TestCase(string name)
     }
 }
 
+public class TestResult
+{
+    public TestResult()
+    {
+        RunCount = 0;
+    }
+    private int RunCount { get; set; }
+
+    public void TestStarted()
+    {
+        RunCount++;
+    }
+
+    public string Summary()
+    {
+        return $"{RunCount} run, 0 failed";
+    }
+}
+
 public class TestCaseTest(string name) : TestCase(name)
 {
     public void TestTemplateMethod()
@@ -33,6 +58,18 @@ public class TestCaseTest(string name) : TestCase(name)
         var test = new WasRun("TestMethod");
         test.Run();
         Assert("SetUp TestMethod TearDown " == test.Log);
+    }
+    public void TestResult()
+    {
+        var test = new WasRun("TestMethod");
+        var result = test.Run();
+        Assert("1 run, 0 failed" == result.Summary());
+    }
+    public void TestFailedResult()
+    {
+        var test = new WasRun("TestBrokenMethod");
+        var result = test.Run();
+        Assert("1 run, 1 failed" == result.Summary());
     }
 }
 
@@ -51,5 +88,9 @@ public class WasRun(string name) : TestCase(name)
     public void TestMethod()
     {
         Log += "TestMethod ";
+    }
+    public void TestBrokenMethod()
+    {
+        throw new Exception();
     }
 }
